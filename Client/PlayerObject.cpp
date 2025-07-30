1554e0fad64eb59f6df740126615b7443ddb8b63
ddhhssjj <can.thcha@gmail.com>
2025-07-30 19:43:19 +0900

    flip 구현

--- Client/PlayerObject.cpp
+++ Client/PlayerObject.cpp
@@ -6,20 +6,21 @@
 
 void PlayerObject::Update(float deltaTime)
 {
-	//static float time = 0;
+	static float time = 0;
 
-	//time += deltaTime;
+	time += deltaTime;
 
-	//if (time >= 1.0f)
-	//{
-	//	time -= 1.0f;
-	//	SpriteRenderer* sprite = GetComponent<SpriteRenderer>();
+	if (time >= 1.0f)
+	{
+		time -= 1.0f;
+		SpriteRenderer* sprite = GetComponent<SpriteRenderer>();
 
-	//	//if (&it != nullptr)
-	//	//{
-	//		sprite->SetFlipX(!sprite->GetFlipX());
-	//	//}
-	//}
+		//if (&it != nullptr)
+		//{
+			sprite->SetFlipX(!sprite->GetFlipX());
+			sprite->SetFlipY(!sprite->GetFlipY());
+		//}
+	}
 
 	GameObject::Update(deltaTime);
 }
@@ -29,22 +30,63 @@ void PlayerObject::Render(std::vector<RenderInfo>& renderInfo)
 	auto sprite = GetComponent<SpriteRenderer>();
 	auto image = GetComponent<UIImageComponent>();
 
+	float z = GetComponent<RunPlayerController>()->GetZ();
+
 	if (sprite)
 	{
-		RenderInfo info;
-		info.bitmap = sprite->GetTexture();
-		info.worldMatrix = m_Transform->GetWorldMatrix() * D2D1::Matrix3x2F::Translation(0, GetComponent<RunPlayerController>()->GetZ());
-		info.size = { 1,1 };
-		if (sprite->GetFlipX())
-			info.size = { -1, 1 };
-		info.pivot = sprite->GetPivot();
-		// UI가 아닌 일반 오브젝트 위치로 설정
-		info.anchor = Anchor{ {0.0f, 0.0f}, {0.0f, 0.0f} }; // (0,0)-(0,0) 고정값
-		info.anchoredPosition = m_Transform->GetPosition();
-		info.sizeDelta = { 0, 0 };
-		info.parentSize = { 0, 0 };
-		renderInfo.push_back(info);
-		
+		{
+			RenderInfo info;
+			info.bitmap = sprite->GetTexture();
+			D2D1::Matrix3x2F flip = D2D1::Matrix3x2F::Identity();
+			if (sprite->GetFlipX())
+			{
+				flip._11 = -1;
+			}
+			else
+			{
+				flip._11 = 1;
+			}
+			if (sprite->GetFlipY())
+			{
+				flip._22 = -1;
+			}
+			else
+			{
+				flip._22 = 1;
+			}
+			info.worldMatrix = flip * D2D1::Matrix3x2F::Translation(0, z) *  m_Transform->GetWorldMatrix();
+			info.size = { 1,1 };
+			if (sprite->GetFlipX())
+				info.size = { -1, 1 };
+			info.pivot = sprite->GetPivot();
+			// UI가 아닌 일반 오브젝트 위치로 설정
+			info.anchor = Anchor{ {0.0f, 0.0f}, {0.0f, 0.0f} }; // (0,0)-(0,0) 고정값
+			info.anchoredPosition = m_Transform->GetPosition();
+			info.sizeDelta = { 0, 0 };
+			info.parentSize = { 0, 0 };
+			renderInfo.push_back(info);
+		}
+		{
+			RenderInfo info;
+			info.bitmap = sprite->GetTexture();
+			Math::Vector2F pos = m_Transform->GetPosition();
+			float y = pos.y;
+			pos.y = z;
+			m_Transform->SetPosition(pos);
+			info.worldMatrix = m_Transform->GetWorldMatrix();
+			info.size = { 1,1 };
+			if (sprite->GetFlipX())
+				info.size = { -1, 1 };
+			info.pivot = sprite->GetPivot();
+			// UI가 아닌 일반 오브젝트 위치로 설정
+			info.anchor = Anchor{ {0.0f, 0.0f}, {0.0f, 0.0f} }; // (0,0)-(0,0) 고정값
+			info.anchoredPosition = m_Transform->GetPosition();
+			info.sizeDelta = { 0, 0 };
+			info.parentSize = { 0, 0 };
+			renderInfo.push_back(info);
+			pos.y = y;
+			m_Transform->SetPosition(pos);
+		}
 	}
 	else if (image)
 	{
--- Client/PlayerObject.h
+++ Client/PlayerObject.h
@@ -2,7 +2,7 @@
 #include "GameObject.h"
 #include "RunPlayerController.h"
 #include "RigidbodyComponent.h"
-//#include "FSMComponent.h"
+#include "FSM.h"
 
 
 //존재 이유 플레이어 생성마다 수많은 FSM 컴포넌트의 상태 설정과, 컴포넌트 생성을 씬에서 하기엔 힘들것 같아 생성시 동시에 컴포넌트를 내부에서 처리하게함
@@ -17,6 +17,7 @@ public:
 		m_RigidbodyComponent = AddComponent<RigidbodyComponent>();
 		m_RigidbodyComponent->Start();
 		//m_FSMComponent = AddComponent<FSMComponent>();
+		m_FSM = std::make_unique<FSM>();
 	}
 	~PlayerObject()
 	{
@@ -31,5 +32,6 @@ public:
 	RunPlayerController* m_Controller;
 	RigidbodyComponent* m_RigidbodyComponent;// 혹시 몰라 쓰는 포인터들 비용 커질 것 같으면 지워도 됩니다
 	//FSMComponent* m_FSMComponent;
+	std::unique_ptr<FSM> m_FSM;
 };
 
