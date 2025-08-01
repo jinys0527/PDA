7516298ed79d53dd0d7d7fb97e3263a9b92a8d4a
DotheZac <128376783+DotheZac@users.noreply.github.com>
2025-08-01 15:01:48 +0900

    UIObject쪽에 리스너 있던거 지움

--- GameObject/UIObject.cpp
+++ GameObject/UIObject.cpp
@@ -5,13 +5,9 @@ UIObject::UIObject(EventDispatcher& eventDispatcher) : GameObject(eventDispatche
 {
 	//임시로 버튼 컴포넌트 넣음
 	m_UI = AddComponent<UIButtonComponent>();
-	eventDispatcher.AddListener(EventType::MouseLeftClick, m_UI);
-	eventDispatcher.AddListener(EventType::Hovered, m_UI);
 }
 
 UIObject::~UIObject()
 {
-	m_EventDispatcher.RemoveListener(EventType::MouseLeftClick, m_UI);
-	m_EventDispatcher.RemoveListener(EventType::Hovered, m_UI);
 }
 
