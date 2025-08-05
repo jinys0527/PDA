8d06db091dad2ddc486491b8572134df820e803c
ddhhssjj <can.thcha@gmail.com>
2025-08-05 10:43:14 +0900

    박스 충돌 컴포넌트
    박스 충돌 컴포넌트 트랜스폼 크기에 따라 히트박스 크기 달리지게 함

--- Event/IEventListener.h
+++ Event/IEventListener.h
@@ -21,6 +21,10 @@ enum class EventType
 	CollisionExit,
 
 	//Game
+	OnPlayerCollisonOccur, // 플레이어 충돌 플레이어 피격 전 무적 판정 등 체크
+	OnPlayerHit, // 플레이어 진짜 피격
+	OnPlayerDeath, // 플레이어 죽음
+
 };
 
 class IEventListener
